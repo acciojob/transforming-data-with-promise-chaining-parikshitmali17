52be97b20083df1cd2def48a61d5d3aa8fcffaf0
Parikshit Mali <parikshitmali17@gmail.com>
2025-04-26 20:05:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+// Get the elements
+const input = document.getElementById('ip');
+const button = document.getElementById('btn');
+const output = document.getElementById('output');
+
+// Function to create a delayed promise
+function delayedPromise(value, delay, operation) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const newValue = operation(value);
+      resolve(newValue);
+    }, delay);
+  });
+}
+
+// Handle button click
+button.onclick = function () {
+  const inputValue = Number(input.value);
+
+  // Start the promise chain
+  delayedPromise(inputValue, 2000, (val) => val)
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedPromise(result, 2000, (val) => val * 2);
+    })
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedPromise(result, 1000, (val) => val - 3);
+    })
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedPromise(result, 1000, (val) => val / 2);
+    })
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedPromise(result, 1000, (val) => val + 10);
+    })
+    .then((finalResult) => {
+      output.innerText = `Final Result: ${finalResult}`;
+    })
+    .catch((error) => {
+      output.innerText = `Error: ${error}`;
+    });
+};
